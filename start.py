40d538795fa1d504bf21af6a5af6c75fbe79dce7
Marc MNilà <milamartosmarc5@gmail.com>
2021-03-26 10:38:42 +0100

    done

--- start.py
+++ start.py
@@ -10,9 +10,9 @@ cmd2 = 'python /home/pi/testbed-v2/Scenario-IoTs/StreetLight/gestion_farolas.py 
 cmd3 = 'python /home/pi/testbed-v2/Scenario-IoTs/Traffic/gestion_trafico.py ip=' + sys.argv[1] + ' port=5004'
 
 
-command = os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/gestion_semaforos.py') and 
-os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/gestion_semaforos.py') and 
-os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/gestion_semaforos.py')
+command = (os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/TrafficLight/gestion_semaforos.py') and 
+os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/StreetLight/gestion_farolas.py') and 
+os.path.exists('/home/pi/testbed-v2/Scenario-IoTs/Traffic/gestion_trafico.py'))
 
 
 if command:
@@ -20,8 +20,9 @@ if command:
     subprocess.Popen(shlex.split(cmd2), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     subprocess.Popen(shlex.split(cmd3), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 else:
-    estan=false
-    while !estan:
+    estan=False
+    print 'no estan'
+    while not estan:
 	if command:
 	    estan=true
 	    subprocess.Popen(shlex.split(cmd1), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
